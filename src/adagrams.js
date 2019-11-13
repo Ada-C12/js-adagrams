2654d650406cfa758edb628c9c435693b7ce14c8
stupendousC <CarolineWuKaplan@gmail.com>
2019-11-13 14:58:04 -0800

    wave1 works

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,39 +1,74 @@
 const Adagrams = {
-  newBag: {
-    "A":9,
-    "B":2;
-    "C":2;
-    "D":4;
-    "E":12;
-    "F":2;
-    "G":3;
-    "H":2;
-    "I":9;
-    "J":1;
-    "K":1;
-    "L":4;
-    "M":2;
-    "N":6;
-    "O":8;
-    "P":2;
-    "Q":1;
-    "R":6;
-    "S":4;
-    "T":6;
-    "U":4;
-    "V":2;
-    "W":2;
-    "X":1;
-    "Y":2;
-    "Z":1;
-  };
+  frequencies: [
+    ["A",9],
+    ["B", 2],
+    ["C", 2],
+    ["D", 4],
+    ["E", 12],
+    ["F", 2],
+    ["G", 3],
+    ["H", 2],
+    ["I", 9],
+    ["J", 1],
+    ["K", 1],
+    ["L", 4],
+    ["M", 2],
+    ["N", 6],
+    ["O", 8],
+    ["P", 2],
+    ["Q", 1],
+    ["R", 6],
+    ["S", 4],
+    ["T", 6],
+    ["U", 4],
+    ["V", 2],
+    ["W", 2],
+    ["X", 1],
+    ["Y", 2],
+    ["Z", 1],
+  ],
+
+  makeNewBag() {
+    let bag = [];
+
+    for (let i=0; i<this.frequencies.length; i++) {
+
+      let entry = this.frequencies[i];
+
+      for (let j=0; j<entry[1]; j++) {
+        bag.push(entry[0]); 
+      }
+    }
+    return bag;
+  },
   
   drawLetters() {
-    // Implement this method for wave 1
-  },
+    // draw 10 letters from newBag, return as array of 10 strings
+    let newBag = this.makeNewBag();
+    let tray = [];
+    // console.log(newBag);
 
+    for (let i=0; i < 10; i++) {
+      let randomIndex = Math.floor(Math.random() * newBag.length);
+
+      // remove from bag...
+      newBag.splice(randomIndex, 1);
+
+      // ...add to player's tray
+      tray.push(newBag[randomIndex]);
+    }
+
+    return tray;
+  },
 
 };
 
+console.log(Adagrams.drawLetters());
+
+
+
+
+
+// REACTIVATE line later!!!
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
