edca99a8dfab9af93818dc3ac0906436cc737242
Janice Huang <janicehuang@gmail.com>
2019-11-13 14:37:12 -0800

    add code to put letters in hash then into array but not yet running

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,49 @@
 const Adagrams = {
+
+  const alphabetObject = {};
+  alphabetObject: {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  const makeLetters = () => {
+  const alphabetArray = [];
+  for (let key in alphabetObject) {
+    let i = alphabetObject[key];
+    while (i > 0) {
+      alphabetArray << key;
+      i -= 1;
+    };
+    console.log(alphabetArray)
+  }}};
   drawLetters() {
-    
   },
 };
+
+makeLetters()
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
