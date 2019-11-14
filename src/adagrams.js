c9bf0655e7a7c3d6cc81555082c5cf09013053b8
gracexinran <gracexinran@gmail.com>
2019-11-13 21:40:42 -0800

    Wave 3 completed

--- src/adagrams.js
+++ src/adagrams.js
@@ -69,7 +69,7 @@ const Adagrams = {
     })
     console.log(lettersObject)
     let result = true
-    input.split('').forEach(function (letter) {
+    input.toUpperCase().split('').forEach(function (letter) {
       if (lettersObject[letter] && lettersObject[letter] > 0) {
         lettersObject[letter]--
       } else {
@@ -77,7 +77,47 @@ const Adagrams = {
       }
     })
     return result
-  }
+  },
+  
+  lettersValue: {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10
+  },
+  
+  scoreWord(word) {
+    let wordPoint = 0
+    word.toUpperCase().split('').forEach(letter => {
+      wordPoint += this.lettersValue[letter]
+    })
+    if (word.length > 6) {
+      wordPoint += 8
+    }
+    return wordPoint
+  },
 };
 
 // Do not remove this line or your tests will break!
