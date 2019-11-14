da81b4fb3b25525041cd5725398618b11c838beb
Erika <erika.maust@gmail.com>
2019-11-14 13:56:19 -0800

    Able to access letter value

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,39 +1,63 @@
-const Adagrams = {
-  drawLetters() {
+// const Adagrams = {
+//   drawLetters() {
+
+    // const letters = {
+    //   "A": 9,
+    //   "B": 2
+    // };
+      // C: 2,
+      // D: 4,
+      // E: 12,
+      // F: 2,
+      // G: 3,
+      // H: 2,
+      // I: 9,
+      // J: 1,
+      // K: 1,
+      // L: 4,
+      // M: 2,
+      // N: 6,
+      // O: 8,
+      // P: 2,
+      // Q: 1,
+      // R: 6,
+      // S: 4,
+      // T: 6,
+      // U: 4,
+      // V: 2,
+      // W: 2,
+      // X: 1,
+      // Y: 2,
+      // Z: 1
+    // }
 
     const letters = {
-      A: 9,
-      B: 2,
-      C: 2,
-      D: 4,
-      E: 12,
-      F: 2,
-      G: 3,
-      H: 2,
-      I: 9,
-      J: 1,
-      K: 1,
-      L: 4,
-      M: 2,
-      N: 6,
-      O: 8,
-      P: 2,
-      Q: 1,
-      R: 6,
-      S: 4,
-      T: 6,
-      U: 4,
-      V: 2,
-      W: 2,
-      X: 1,
-      Y: 2,
-      Z: 1
-    }
+      "A": 9,
+      "B": 2
+    };
+    
+    for (let element in letters) {
+      console.log(letters[element])
+      count = element.value + 1
+      let letterSet = []
+      do {
+        letters.value - 1
+        letterSet.push(letters.key);
+      } while (count > 0)
+    };
+      
+
+      
+
     // Array of all letters of the alphabet
-    // 
+    // Take key from the letters object
+    // Make the quantity of occurances of that key the number stored in the value
+    // Map to array
     // Draws 10 letters 
-  },
-};
+    // If the letter is drawn, the value should decrease by one
+    // A letter cannot be drawn if the value is 0
+  // },
+// };
 
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
