82f433e1e0951126cafec024c261a03daa4cf57f
Janice Huang <janicehuang@gmail.com>
2019-11-13 22:34:01 -0800

    make progress

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,49 +1,86 @@
-const Adagrams = {
-
-  const alphabetObject = {};
-  alphabetObject: {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1,
-  };
+// const Adagrams = {
   const makeLetters = () => {
-  const alphabetArray = [];
-  for (let key in alphabetObject) {
-    let i = alphabetObject[key];
-    while (i > 0) {
-      alphabetArray << key;
-      i -= 1;
+    const alphabetObject = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1,
     };
-    console.log(alphabetArray)
-  }}};
-  drawLetters() {
-  },
-};
 
-makeLetters()
+    const alphabetArray = [];
+    for (let key in alphabetObject) {
+      let i = alphabetObject[key];
+      while (i > 0) {
+        alphabetArray.push(key);
+        i -= 1;
+      };
+    };
+  return alphabetArray;
+  };
+// adapted from Fisher-Yates Shuffle
+  const shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+  }
 
+  const drawLetters = () => {
+    let shuffled = shuffle(makeLetters());
+    let lettersInHand = shuffled.slice(0, 10);
+    return lettersInHand;
+  }
+
+  const usesAvailableLetters = function(input, lettersInHand) {
+    let handCollection = {}
+    lettersInHand.forEach((letter) => {
+      if (handCollection[letter] == null) {
+        handCollection[letter] = 1
+      }
+      else {
+        handCollection[letter] += 1
+    }
+  })
+    input.text.toUpperCase().split('').forEach((letter) => {
+      if (handCollection.keys.includes(letter)) {
+        handCollection[letter] -= 1
+      }
+})
+  }
+    //put all letters w count in a "hash", then check input against
+
+
+  // },
+// };
+// use THIS to call make letters once i put them both within Adagrams
+
+usesAvailableLetters('test', drawLetters())
 // Do not remove this line or your tests will break!
-export default Adagrams;
+// export default Adagrams;
