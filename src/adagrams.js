8ba106cca88193870f345bd48d3c143a4078746a
Mira <calopter@gmail.com>
2019-11-14 13:25:02 -0800

    scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -6,6 +6,29 @@ const Adagrams = {
                 J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2,
                 M: 2, Z: 1 },
 
+  scores: { A: 1, N: 1, B: 3, O: 1, C: 3, P: 3,
+            D: 2, Q: 10, E: 1, R: 1, F: 4, S: 1,
+            G: 2, T: 1, H: 4, U: 1, I: 1, V: 4, 
+            J: 8, W: 4, K: 5, X: 8, L: 1, Y: 4,
+            M: 3, Z: 10 },
+
+  scoreWord(word) {
+    const scoreLetter = (letter) => this.scores[letter];
+    const sum = (total, score) => total + score;
+    let score = 0;
+
+    if (word.length > 0) {
+      score += word.toUpperCase().split('')
+                   .map(scoreLetter).reduce(sum);
+    }
+
+    if (word.length >= 7) {
+      score += 8;
+    }
+
+    return score;
+  },
+
   usesAvailableLetters(word, drawn) {
     let hand = {};
     
