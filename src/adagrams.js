5c37852174d793b02b06336b1cb17372d44e548a
Dianna Tingg <dtingg@gmail.com>
2019-11-13 17:03:54 -0800

    finished uses available letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -18,28 +18,25 @@ allLetters.sort(function() { return 0.5 - Math.random() });
 
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
     return allLetters.slice(0, 10);
   },
-};
+  usesAvailableLetters(input, lettersInHand) {
+    let validWord = true;
+    let userWord = input.toUpperCase().split("");
 
+    userWord.forEach((char) => {
+      let index = lettersInHand.indexOf(char);
 
-console.log(Adagrams.drawLetters())
-
-
-// •	Returns an array of ten strings
-// o	Each string should contain exactly one letter
-// o	These represent the hand of letters that the player has drawn
-// •	The letters should be randomly drawn from a pool of letters
-// o	This letter pool should reflect the distribution of letters as described in the table below
-// o	There are only 2 available C letters, so drawLetters cannot ever return more than 2 Cs
-// o	Since there are 12 Es but only 1 Z, it should be 12 times as likely to draw an E as a Z
-// •	Invoking this function should not change the pool of letters
-// o	Imagine that the user returns their hand to the pool before drawing new letters
-
-
-
-
+      if (index === -1) {
+        validWord = false;
+        return;
+      } else {
+        lettersInHand.splice(index, 1)
+      }
+    })
+    return validWord;
+  }
+}
 
 
 
