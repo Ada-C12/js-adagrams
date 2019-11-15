95f29e7d054569343e7758597aef05cccefd79bb
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-11-14 21:16:41 -0800

    added usesAvailableLetters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,7 @@
 const Adagrams = {
+ 
   drawLetters() {
-    // Implement this method for wave 1
+// //     // Implement this method for wave 1
     let letterPool = [
     Array(9).fill('A'),Array(2).fill('B'),Array(2).fill('C'),
     Array(4).fill('D'),Array(12).fill('E'),Array(2).fill('F'),
@@ -10,28 +11,53 @@ const Adagrams = {
     Array(2).fill('P'),Array(1).fill('Q'),Array(6).fill('R'),
     Array(4).fill('S'),Array(6).fill('T'),Array(4).fill('U'),
     Array(2).fill('V'),Array(2).fill('W'),Array(1).fill('X'),
-    Array(2).fill('Y'),Array(1).fill('Z')
+    Array(2).fill('Y'),Array(1).fill('Z'),
   
   
-  ].flat(2)
+  ].flat(2);
 
-  let hand = []
-  console.log(letterPool.length)
+  let hand = [];
 
   for (let i = 0; i < 10; i += 1) {
     const randomIndex = Math.floor(Math.random() * 99);
-    hand.push(letterPool[randomIndex])
+    hand.push(letterPool[randomIndex]);
   }
 
-console.log(hand)
 return hand;
 
-    }
-   
+    },
 
+ usesAvailableLetters(word, hand) {
+      let handObj = {};
+      let result = true;
+
+      hand.forEach(function(letter){
+        if(handObj[letter] ){
+          handObj[letter] += 1;
+        }else{
+          handObj[letter] = 1;
+        }
+
+      })
+
+      word.split('').forEach(function(letter){
+        if (handObj[letter]){
+          handObj[letter] -= 1; 
+          if (handObj[letter] == 0) {
+            delete handObj[letter];
+          } 
+        }else {
+          result = false;
+        }
+      })
+    return result; 
+
+    },
 
 
 }
+
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
