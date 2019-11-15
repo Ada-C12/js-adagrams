7cfa5a3791496ffe4f14bd31e17d3a334cbc2444
gracexinran <gracexinran@gmail.com>
2019-11-14 23:34:56 -0800

    Implemented highestScoreFrom function and completed optional wave-4

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,31 +1,31 @@
 const Adagrams = {
   lettersPool: {
-    'A': 9,
-    'B': 2,
-    'C': 2,
-    'D': 4,
-    'E': 12,
-    'F': 2,
-    'G': 3,
-    'H': 2,
-    'I': 9,
-    'J': 1,
-    'K': 1,
-    'L': 4,
-    'M': 2,
-    'N': 6,
-    'O': 8,
-    'P': 2,
-    'Q': 1,
-    'R': 6,
-    'S': 4,
-    'T': 6,
-    'U': 4,
-    'V': 2,
-    'W': 2,
-    'X': 1,
-    'Y': 2,
-    'Z': 1
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
   },
   
   getAllLetters() {
@@ -67,7 +67,7 @@ const Adagrams = {
         lettersObject[letter] = 1
       }
     })
-    console.log(lettersObject)
+    
     let result = true
     input.toUpperCase().split('').forEach(function (letter) {
       if (lettersObject[letter] && lettersObject[letter] > 0) {
@@ -80,32 +80,32 @@ const Adagrams = {
   },
   
   lettersValue: {
-    'A': 1,
-    'B': 3,
-    'C': 3,
-    'D': 2,
-    'E': 1,
-    'F': 4,
-    'G': 2,
-    'H': 4,
-    'I': 1,
-    'J': 8,
-    'K': 5,
-    'L': 1,
-    'M': 3,
-    'N': 1,
-    'O': 1,
-    'P': 3,
-    'Q': 10,
-    'R': 1,
-    'S': 1,
-    'T': 1,
-    'U': 1,
-    'V': 4,
-    'W': 4,
-    'X': 8,
-    'Y': 4,
-    'Z': 10
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
   },
   
   scoreWord(word) {
@@ -113,11 +113,37 @@ const Adagrams = {
     word.toUpperCase().split('').forEach(letter => {
       wordPoint += this.lettersValue[letter]
     })
+
     if (word.length > 6) {
       wordPoint += 8
     }
+
     return wordPoint
   },
+  
+  highestScoreFrom(words) {
+    let winningWord = { word: '', score: 0 }
+    
+    words.forEach(currentWord => {
+      const currentScore = this.scoreWord(currentWord)
+      
+      if (currentScore > winningWord['score']) {
+        winningWord['word'] = currentWord
+        winningWord['score'] = currentScore
+      }
+      
+      else if (currentScore === winningWord['score']) {
+        const currentLength = currentWord.length
+        const winningLength = winningWord['word'].length
+        if ((currentLength < winningLength || currentLength === 10) && winningLength < 10) {
+          winningWord['word'] = currentWord
+          winningWord['score'] = currentScore
+        }
+      }
+    })
+
+    return winningWord
+  }
 };
 
 // Do not remove this line or your tests will break!
