3d2cd2e6926789a999a72c87cfa424c13f7b105b
Michaela Morrow <morrow.michaela@gmail.com>
2019-11-15 15:35:13 -0800

    Converted Adagrams to a class for wave 5

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,37 +1,39 @@
-const Adagrams = {
-  letterPool: {
-    'A': {numberOfTiles: 9, numberOfPoints: 1},
-    'B': {numberOfTiles: 2, numberOfPoints: 3},
-    'C': {numberOfTiles: 2, numberOfPoints: 3},
-    'D': {numberOfTiles: 4, numberOfPoints: 2},
-    'E': {numberOfTiles: 12, numberOfPoints: 1},
-    'F': {numberOfTiles: 2, numberOfPoints: 4},
-    'G': {numberOfTiles: 3, numberOfPoints: 2},
-    'H': {numberOfTiles: 2, numberOfPoints: 4},
-    'I': {numberOfTiles: 9, numberOfPoints: 1},
-    'J': {numberOfTiles: 1, numberOfPoints: 8},
-    'K': {numberOfTiles: 1, numberOfPoints: 5},
-    'L': {numberOfTiles: 4, numberOfPoints: 1},
-    'M': {numberOfTiles: 2, numberOfPoints: 3},
-    'N': {numberOfTiles: 6, numberOfPoints: 1},
-    'O': {numberOfTiles: 8, numberOfPoints: 1},
-    'P': {numberOfTiles: 2, numberOfPoints: 3},
-    'Q': {numberOfTiles: 1, numberOfPoints: 10},
-    'R': {numberOfTiles: 6, numberOfPoints: 1},
-    'S': {numberOfTiles: 4, numberOfPoints: 1},
-    'T': {numberOfTiles: 6, numberOfPoints: 1},
-    'U': {numberOfTiles: 4, numberOfPoints: 1},
-    'V': {numberOfTiles: 2, numberOfPoints: 4},
-    'W': {numberOfTiles: 2, numberOfPoints: 4},
-    'X': {numberOfTiles: 1, numberOfPoints: 8},
-    'Y': {numberOfTiles: 2, numberOfPoints: 4},
-    'Z': {numberOfTiles: 1, numberOfPoints: 10},
-  },
-  drawLetters() {
+class Adagrams {
+  static letterPool () {
+    return {
+      'A': {numberOfTiles: 9, numberOfPoints: 1},
+      'B': {numberOfTiles: 2, numberOfPoints: 3},
+      'C': {numberOfTiles: 2, numberOfPoints: 3},
+      'D': {numberOfTiles: 4, numberOfPoints: 2},
+      'E': {numberOfTiles: 12, numberOfPoints: 1},
+      'F': {numberOfTiles: 2, numberOfPoints: 4},
+      'G': {numberOfTiles: 3, numberOfPoints: 2},
+      'H': {numberOfTiles: 2, numberOfPoints: 4},
+      'I': {numberOfTiles: 9, numberOfPoints: 1},
+      'J': {numberOfTiles: 1, numberOfPoints: 8},
+      'K': {numberOfTiles: 1, numberOfPoints: 5},
+      'L': {numberOfTiles: 4, numberOfPoints: 1},
+      'M': {numberOfTiles: 2, numberOfPoints: 3},
+      'N': {numberOfTiles: 6, numberOfPoints: 1},
+      'O': {numberOfTiles: 8, numberOfPoints: 1},
+      'P': {numberOfTiles: 2, numberOfPoints: 3},
+      'Q': {numberOfTiles: 1, numberOfPoints: 10},
+      'R': {numberOfTiles: 6, numberOfPoints: 1},
+      'S': {numberOfTiles: 4, numberOfPoints: 1},
+      'T': {numberOfTiles: 6, numberOfPoints: 1},
+      'U': {numberOfTiles: 4, numberOfPoints: 1},
+      'V': {numberOfTiles: 2, numberOfPoints: 4},
+      'W': {numberOfTiles: 2, numberOfPoints: 4},
+      'X': {numberOfTiles: 1, numberOfPoints: 8},
+      'Y': {numberOfTiles: 2, numberOfPoints: 4},
+      'Z': {numberOfTiles: 1, numberOfPoints: 10},
+    };
+  }
+  static drawLetters() {
     let pool = [];
-    for ( let key in this.letterPool) {
+    for ( let key in this.letterPool()) {
       let i = 0;
-      while (i < this.letterPool[key].numberOfTiles) {
+      while (i < this.letterPool()[key].numberOfTiles) {
         pool.push(key);
         i += 1
       }
@@ -45,8 +47,8 @@ const Adagrams = {
       i += 1;
     }
     return drawnLetters;
-  },
-  usesAvailableLetters(input, lettersInHand) {
+  }
+  static usesAvailableLetters(input, lettersInHand) {
     let copyLettersInHand = [...lettersInHand];
     
     for(let i = 0; i < input.length; i++) {
@@ -58,8 +60,8 @@ const Adagrams = {
       }
     }
     return true;
-  },
-  scoreWord(word) {
+  }
+  static scoreWord(word) {
     let sum = 0;
 
     if (word.length >= 7) {
@@ -68,11 +70,11 @@ const Adagrams = {
 
     word = word.toUpperCase();
     for(let i = 0; i < word.length; i++) {
-      sum += this.letterPool[word[i]].numberOfPoints;
+      sum += this.letterPool()[word[i]].numberOfPoints;
     }
     return sum;
-  },
-  highestScoreFrom(words) {
+  }
+  static highestScoreFrom(words) {
     let max = 0;
     let result = {};
     const that = this;
