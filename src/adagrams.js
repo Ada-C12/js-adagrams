633e2baa4cfe1c1e7412c8bd8af6ee8ce82f5429
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-11-14 21:27:38 -0800

    Added some comments

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,26 +2,30 @@ class Adagrams {
   
   static drawLetters() {
     const letters = { 'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1 };
-    const keys = Object.keys(letters);
-    const strings = new Array;
+
+    const drawnLetters = new Array;
+
     for (let i = 0; i < 10; i++) {
       const drawOneLetter = () => {
-        const key = keys[Math.floor(Math.random() * 100) % 26];
+        const key = Object.keys(letters)[Math.floor(Math.random() * 100) % 26];
+        // if there are available letters, decrement this letter by 1
+        // else, re-draw a letter
         if (letters[key] > 0) {
           letters[key] -= 1;
-          strings.push(key)
+          drawnLetters.push(key);
         } else {
           drawOneLetter();
         }
       }
       drawOneLetter();
     }
-    return strings;
+    return drawnLetters;
   }
 
   static usesAvailableLetters(input, lettersInHand) {
     input = input.toUpperCase();
     let result = true;
+
     input.split("").forEach(function (letter) {
       if (lettersInHand.includes(letter) && result != false) {
         lettersInHand.splice(lettersInHand.indexOf(letter), 1)
@@ -30,15 +34,18 @@ class Adagrams {
         result = false;
       }
     });
+
     return result;
   }
 
   static scoreWord(word) {
+
     let points = 0;
     word = word.toUpperCase().split("")
     if (word.length >= 7) {
       points += 8
     }
+
     const letterValues = {
       1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
       2: ['D', 'G'],
@@ -48,6 +55,7 @@ class Adagrams {
       8: ['J', 'X'],
       10: ['Q','Z']
     }
+
     word.forEach(function (letter) {
       Object.keys(letterValues).forEach(function (key) {
         if (letterValues[key].includes(letter)) {
@@ -55,6 +63,7 @@ class Adagrams {
         }
       })
     })
+
     return points;
   }
 
@@ -74,12 +83,13 @@ class Adagrams {
         } else if (word.length < winningWord.word.length && winningWord.word.length != 10) {
           winningWord = { word: word, score: wordsWithScores[word] }
         }
+        // (if neither of these execute: current winning word is already 10 letters long, and in event of a two 10 letter word tie, winning word is already the first word played and wins the tie.)
       }
+      // (if neither of these execute: current winning word persists)
     });
     return winningWord;
   }
 }
 
-// console.log(Adagrams.highestScoreFrom(["fff", "a", "ddd", "hhhhhbbba", "bbbbbbbbbb"]));
 // Do not remove this line or your tests will break!
 export default Adagrams;
