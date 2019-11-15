131f82e58dba0abe1ec32231a3b9c0b9470fb71e
gracexinran <gracexinran@gmail.com>
2019-11-15 00:06:11 -0800

    Changed object 'Adagrams' to class 'Adagram', completed optional wave-5

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,42 +1,43 @@
-const Adagrams = {
-  lettersPool: {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1
-  },
+class Adagram {
   
   getAllLetters() {
+    const lettersPool = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1
+    }
+
     let lettersArray = []
-    for (const letter in this.lettersPool) {
-      for (let i = 0; i < this.lettersPool[letter]; i++){
+    for (const letter in lettersPool) {
+      for (let i = 0; i < lettersPool[letter]; i++){
         lettersArray.push(letter)
       }
     }
     return lettersArray
-  },
+  }
   
   drawLetters() {
     // Implement this method for wave 1
@@ -56,7 +57,7 @@ const Adagrams = {
       letters.pop()
     }
     return myLetters
-  },
+  }
   
   usesAvailableLetters(input, lettersInhand) {
     let lettersObject = {}
@@ -77,41 +78,41 @@ const Adagrams = {
       }
     })
     return result
-  },
-  
-  lettersValue: {
-    A: 1,
-    B: 3,
-    C: 3,
-    D: 2,
-    E: 1,
-    F: 4,
-    G: 2,
-    H: 4,
-    I: 1,
-    J: 8,
-    K: 5,
-    L: 1,
-    M: 3,
-    N: 1,
-    O: 1,
-    P: 3,
-    Q: 10,
-    R: 1,
-    S: 1,
-    T: 1,
-    U: 1,
-    V: 4,
-    W: 4,
-    X: 8,
-    Y: 4,
-    Z: 10
-  },
+  }
   
   scoreWord(word) {
+    const lettersValue = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10
+    }
+
     let wordPoint = 0
     word.toUpperCase().split('').forEach(letter => {
-      wordPoint += this.lettersValue[letter]
+      wordPoint += lettersValue[letter]
     })
 
     if (word.length > 6) {
@@ -119,7 +120,7 @@ const Adagrams = {
     }
 
     return wordPoint
-  },
+  }
   
   highestScoreFrom(words) {
     let winningWord = { word: '', score: 0 }
@@ -144,7 +145,8 @@ const Adagrams = {
 
     return winningWord
   }
-};
+}
 
+const Adagrams = new Adagram
 // Do not remove this line or your tests will break!
 export default Adagrams;
