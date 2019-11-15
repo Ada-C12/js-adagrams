c23a99c85bf961835972a3891decebc7c8476563
Kristina <kristina.tanya@gmail.com>
2019-11-15 13:52:49 -0800

    wave 1 and wave 2 tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,93 @@
+// move letterPool outside of the function
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    // create a object/hash with letters and their counts
+    const letterPool = {
+      'A': 9,
+      'B': 2,
+      'C': 2,
+      'D': 4,
+      'E': 12,
+      'F': 2,
+      'G': 3,
+      'H': 2,
+      'I': 9, 
+      'J': 1,
+      'K': 1,
+      'L': 4,
+      'M': 2,
+      'N': 6,
+      'O': 8,
+      'P': 2,
+      'Q': 1,
+      'R': 6,
+      'S': 4,
+      'T': 6,
+      'U': 4,
+      'V': 2,
+      'W': 2,
+      'X': 1,
+      'Y': 2,
+      'Z': 1,
+    }
+    // iterate through object and add keys to an array x amt of times depending
+    // on their quantity
+    let letterPoolList = [];
+
+    for (let letter in letterPool) {
+      for (let x = 0; x < letterPool[letter]; x+=1) {
+        letterPoolList.push(letter);
+      }
+    }
+
+    letterPoolList.sort(() => Math.random());
+    
+    letterPoolList = letterPoolList.slice(0,10);
+
+    return letterPoolList;
   },
+  usesAvailableLetters(word, drawn) {
+    let valid
+    const wordArray = word.split('')
+    
+    for (const letter in wordArray) {
+      if (drawn.includes(wordArray[letter]) === false) {
+        console.log('Kristina')
+        valid = false;
+        return valid;
+      }
+      else {
+        console.log(drawn);
+        let index = drawn.indexOf(wordArray[letter]);
+        console.log( `Index ${index}: Letter ${letter}`);
+        drawn.splice(index, 1);
+        // console.log(drawn);
+        valid = true;
+      }
+    }
+
+    // wordArray.forEach(function(letter) {
+    //   if (drawn.includes(letter) === false) {
+    //     valid = false;
+    //   }
+    //   else {
+    //     let index = drawn.indexOf(letter);
+    //     console.log(index);
+    //     drawn.splice(index, 1);
+    //     console.log(drawn);
+    //     valid = true;
+    //   }
+    // });
+
+    return valid;
+  },
+
 };
+
+
+
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -45,14 +45,14 @@ describe('Adagrams', () => {
     });
   });
 
-  describe('scoreWord', () => {
+  describe.skip('scoreWord', () => {
     const expectScores = (wordScores) => {
       Object.entries(wordScores).forEach(([word, score]) => {
         expect(Adagrams.scoreWord(word)).toBe(score);
       });
     };
 
-    it('returns an accurate numerical score according to the score chart', () => {
+    it.skip('returns an accurate numerical score according to the score chart', () => {
       expectScores({
         A: 1,
         DOG: 5,
@@ -60,7 +60,7 @@ describe('Adagrams', () => {
       });
     });
 
-    it('returns a score regardless of the input case', () => {
+    it.skip('returns a score regardless of the input case', () => {
       expectScores({
         a: 1,
         dog: 5,
