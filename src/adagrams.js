2ce47278189b93dd38b3fdac148a5a2f2f9ca621
Janice Huang <janicehuang@gmail.com>
2019-11-15 13:59:17 -0800

    all tests passing, through required waves

--- src/adagrams.js
+++ src/adagrams.js
@@ -51,7 +51,49 @@ const shuffle = function(array) {
     array[randomIndex] = temporaryValue;
   }
   return array;
+};
+
+const scoreWord = function(word) {
+  const scoreboard = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
   };
+  let score = 0;
+  if (word.length >= 7) {
+    score += 8;
+  };
+  word = word.toUpperCase().split('');
+  word.forEach((letter) => {
+    if (Object.keys(scoreboard).includes(letter)) {
+        score += scoreboard[letter];
+    }
+  })
+return score;
+};
 
 const Adagrams = {
   makeLetters,
@@ -92,47 +134,7 @@ const Adagrams = {
     return true
   },
 
-  scoreWord(word) {
-    const scoreboard = {
-      A: 1,
-      B: 3,
-      C: 3,
-      D: 2,
-      E: 1,
-      F: 4,
-      G: 2,
-      H: 4,
-      I: 1,
-      J: 8,
-      K: 5,
-      L: 1,
-      M: 3,
-      N: 1,
-      O: 1,
-      P: 3,
-      Q: 10,
-      R: 1,
-      S: 1,
-      T: 1,
-      U: 1,
-      V: 4,
-      W: 4,
-      X: 8,
-      Y: 4,
-      Z: 10,
-    };
-    let score = 0;
-    if (word.length >= 7) {
-      score += 8;
-    };
-    word = word.toUpperCase().split('');
-    word.forEach((letter) => {
-      if (Object.keys(scoreboard).includes(letter)) {
-          score += scoreboard[letter];
-      }
-    })
-  return score;
-  }
+  scoreWord,
 }
 
 
