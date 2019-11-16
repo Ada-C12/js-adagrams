b1a8f121003a6a32b683850a2da1cd465969ec2a
Ga-Young Jin <gyjinn@gmail.com>
2019-11-16 13:15:13 -0800

    completed wave 3, all tests passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -14,6 +14,7 @@ const Adagrams = {
     allLetters = shuffle(allLetters);
 
     return allLetters.slice(0, 10);
+
   },
   usesAvailableLetters(input, lettersInHand) {
     // const lettersInHand = lettersInHand;
@@ -36,6 +37,19 @@ const Adagrams = {
       }
     }
     return true;
+  },
+
+  scoreWord(word) {
+    let score = 0;
+    word.split('').forEach( function(letter) {
+      score += scoreChart[letter.toUpperCase()];
+    });
+
+    if (word.length > 6) {
+      score += 8;
+    }
+
+    return score;
   },
 };
 
@@ -80,4 +94,33 @@ const letterDist = {
   X: 1,
   Y: 2,
   Z: 1
+}
+
+const scoreChart = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1, 
+  N: 1, 
+  R: 1, 
+  S: 1, 
+  T: 1,
+  D: 2, 
+  G: 2,
+  B: 3, 
+  C: 3, 
+  M: 3, 
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5, 
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10
 }
