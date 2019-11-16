996e859c9e004c90a2957e04e6de4289f547119c
Dominique <dom_taylor@icloud.com>
2019-11-16 13:27:01 -0800

    wordDowncase method added

--- src/adagrams.js
+++ src/adagrams.js
@@ -44,21 +44,21 @@ const Adagrams = {
 
     const hand = [];
 
-    let i = 0
+    let i = 0;
 
     while ( i <= 9) {
       hand.push(letterPool[Math.floor(Math.random()*letterPool.length)]);
       i += 1
     }
-    return hand
+    return hand;
   }, 
 
   usesAvailableLetters(input, lettersInHand) {
-    let lettersAvailable = true
+    let lettersAvailable = true;
 
     const checkForLetter = function(letter) {
       if (lettersInHand.includes(letter)) {
-        let index = lettersInHand.indexOf(letter)
+        let index = lettersInHand.indexOf(letter);
         lettersInHand.splice(index, 1);
         
       } else {
@@ -69,8 +69,62 @@ const Adagrams = {
     }
 
     input.split('').forEach(checkForLetter);
-    return lettersAvailable
-  }
+    return lettersAvailable;
+  },
+
+
+
+  scoreWord(word) {
+    const scoreChart = {
+      "a": 1,
+      "e": 1,
+      "i": 1,
+      "o": 1,
+      "u": 1,
+      "l": 1,
+      "n": 1,
+      "r": 1,
+      "s": 1,
+      "t": 1,
+      "d": 2,
+      "g": 2,
+      "b": 3,
+      "c": 3,
+      "m": 3,
+      "p": 3,
+      "f": 4,
+      "h": 4,
+      "v": 4,
+      "w": 4,
+      "y": 4,
+      "k": 5,
+      "j": 8,
+      "x": 8,
+      "q": 10,
+      "z": 10,
+    };
+
+    let wordScore = 0;
+    const wordDowncase = word.toLowerCase();
+  
+    wordDowncase.split('').forEach( function(letter) {
+      if (scoreChart[letter]) {
+        wordScore += scoreChart[letter];
+      };
+    });
+
+    return wordScore
+
+
+    // Make a score chart hash. 
+    // Create a variable called wordScore
+    // if given empty string, return a score of 0
+    // Iterate through the word and if hash[letter], assign the value of that letter to the 
+    // variable wordScore
+    // additional points for length of the word can be added on later. 
+    // calculate the length of the word and assign additional points if necessary
+  },
+
 }
 
 // Do not remove this line or your tests will break!
