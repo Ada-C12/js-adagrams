33e27811a7798c3b600995c06e764b62956623db
Dominique <dom_taylor@icloud.com>
2019-11-16 11:57:37 -0800

    completed wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,7 @@
 const Adagrams = {
+  
   drawLetters() {
-    const letter_pool = {
+    const rawTiles = {
       A: 9, 
       B: 2, 
       C: 2, 
@@ -26,24 +27,51 @@ const Adagrams = {
       W: 2, 
       X: 1, 
       Y: 2, 
-      Z: 1}
+      Z: 1};
 
-      const keys = Object.keys(letter_pool)
-      
-      
+    const keys = Object.keys(rawTiles);
 
+    const letterPool = [];
 
+    for(const key in rawTiles) {
+      let i = 0
 
+      while (i <= rawTiles[key]) {
+        letterPool.push(key);
+        i += 1
+      };
+    };
 
+    const hand = [];
 
+    let i = 0
 
-    
-    
-    
-    
-    const hand = ["a", "b", "i", "d", "e", "o", "z", "h", "i", "m"]
-  },
-};
+    while ( i <= 9) {
+      hand.push(letterPool[Math.floor(Math.random()*letterPool.length)]);
+      i += 1
+    }
+    return hand
+  }, 
+
+  usesAvailableLetters(input, lettersInHand) {
+    let lettersAvailable = true
+
+    const checkForLetter = function(letter) {
+      if (lettersInHand.includes(letter)) {
+        let index = lettersInHand.indexOf(letter)
+        lettersInHand.splice(index, 1);
+        
+      } else {
+        // console.log(`Letter is ${letter}`);
+        // console.log(`lettersInHand is ${lettersInHand}`);
+        lettersAvailable = false;
+      }
+    }
+
+    input.split('').forEach(checkForLetter);
+    return lettersAvailable
+  }
+}
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
