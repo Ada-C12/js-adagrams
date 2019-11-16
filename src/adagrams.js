0c865dc37a285e302988c9e4631dde1893f7fe87
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-11-16 12:47:32 -0800

    Overlooked wave 1 requirement that player hand can't have higher count of a letter than is in letter pool. Added this logic to function. Passing wave 1 tests.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,23 +1,39 @@
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
-    const letterPool = {'A':9, 'B':2, 'C':2, 'D':4, 'E':12, 'F':2, 'G':3, 'H':2, 'I':9, 'J':1, 'K':1, 'L':4, 'N':6, 'O':8, 'P':2, 'Q':1, 'R':6, 'S':4, 'T':6,'U':4, 'V':2, 'W':2, 'X':1, 'Y':2, 'Z':1 }
+    const letterPoolHash = {'A':9, 'B':2, 'C':2, 'D':4, 'E':12, 'F':2, 'G':3, 'H':2, 'I':9, 'J':1, 'K':1, 'L':4, 'N':6, 'O':8, 'P':2, 'Q':1, 'R':6, 'S':4, 'T':6,'U':4, 'V':2, 'W':2, 'X':1, 'Y':2, 'Z':1 };
 
-    
+    const letterPoolArray =  ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B',
+    'C', 'C', 'D', 'D', 'D', 'D', 'E', 'E', 'E', 'E', 'E',
+    'E', 'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'G', 'G',
+    'G', 'H', 'H', 'I', 'I', 'I', 'I', 'I', 'I', 'I', 'I',
+    'I', 'J', 'K', 'L', 'L', 'L', 'L', 'N', 'N', 'N', 'N',
+    'N', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'P',
+    'P', 'Q', 'R', 'R', 'R', 'R', 'R', 'R', 'S', 'S', 'S',
+    'S', 'T', 'T', 'T', 'T', 'T', 'T', 'U', 'U', 'U', 'U',
+    'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z'];
 
     let lettersInHand = [];
-
-    // add logic for re-drawing a letter if total count of letters in pool have already been drawn
     
-    for (let index = 0; index < 10; index++) {
-      // code below courtesy of https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
-      let randomLetter = letterPool[Math.floor(Math.random() * letterPool.length)];
-      lettersInHand.push(randomLetter)
-    }
-    return playerHand
+    // turn draw letter into an anonymous function? or a helper function?
+    
+    // continue this loop until there are ten letters in the player hand 
+    for (let index = 0; lettersInHand.length < 10; index++) {
+      // code for random courtesy of https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+      // pull a random letter from the letter pool array
+      let randomLetter = letterPoolArray[Math.floor(Math.random() * letterPoolArray.length)];
+      // condition: if adding the letter to the player hand would exceed the count of letters in the pool, redraw 
+      if (lettersInHand[randomLetter] == letterPoolHash[randomLetter]) {
+          randomLetter = letterPoolArray[Math.floor(Math.random() * letterPoolArray.length)];
+        } else { // add the letter to player hand array
+          lettersInHand.push(randomLetter)
+        }
+      }
+    return lettersInHand
   }
+  
 
-  usesAvailableLetters(input, lettersInHand)
+  // usesAvailableLetters(input, lettersInHand)
 
 };
 
