8a6014a049d3f57088d1eccaee96578da0fa5913
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-16 15:07:05 -0800

    fixing styling

--- src/adagrams.js
+++ src/adagrams.js
@@ -10,7 +10,7 @@ const Adagrams = {
   },
 
   usesAvailableLetters(input, lettersInHand) {  
-    let result = true
+    let result = true;
     
     for (let i = 0; i < (input.length); i++) {
       if (lettersInHand.includes(input[i]) === false) {
@@ -19,31 +19,34 @@ const Adagrams = {
         lettersInHand.splice(lettersInHand.indexOf(input[i]), 1);
       }
     }
-    return result
+    return result;
   },
 
   scoreWord(word) {
-    const score = { A:1, E:1, I:1, O:1, U:1, L:1, N:1, R:1, S:1, T:1, D:2, G:2, B:3, C:3, M:3, P:3, F:4, H:4, V:4, W:4, Y:4, K:5, J:8, X:8, Q:10, Z:10 } 
-    let points = 0
+    const score = { A:1, E:1, I:1, O:1, U:1, L:1, N:1, R:1, S:1, T:1, D:2, G:2, 
+    B:3, C:3, M:3, P:3, F:4, H:4, V:4, W:4, Y:4, K:5, J:8, X:8, Q:10, Z:10 }; 
 
-    let wordCheck = word.toUpperCase();
+    let points = 0;
+    const wordCheck = word.toUpperCase();
 
     if (wordCheck.length > 6) {
       points += 8;
     }
 
     for (let i = 0; i < (wordCheck.length); i++) {
-        points += score[wordCheck[i]]
+        points += score[wordCheck[i]];
     }
-      return points
+      return points;
   },
 };
 
-// console.log(Adagrams.scoreWord('AVAILABLE'))
+// console.log(Adagrams.drawLetters());
+
 // console.log(Adagrams.usesAvailableLetters('APPLE', ['A', 'P', 'P', 'L', 'E']));
 // console.log(Adagrams.usesAvailableLetters('APPLES', ['A', 'P', 'P', 'L', 'E']));
 
-// console.log(Adagrams.drawLetters());
+// console.log(Adagrams.scoreWord('AVAILABLE'))
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
