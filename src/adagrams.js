2affa00ef333e8f5fde23e67ba1255f9b63a0706
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-16 14:52:42 -0800

    created usesavailableletters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -9,6 +9,25 @@ const Adagrams = {
     return lettersInHand;
   },
 
+  usesAvailableLetters(input, lettersInHand) {  
+    let result = true
+    
+    for (let i = 0; i < (input.length); i++) {
+      if (lettersInHand.includes(input[i]) === false) {
+        result = false;
+      } else {
+        lettersInHand.splice(lettersInHand.indexOf(input[i]), 1);
+      }
+    }
+    return result
+  },
+
+};
+
+// console.log(Adagrams.scoreWord('AVAILABLE'))
+// console.log(Adagrams.usesAvailableLetters('APPLE', ['A', 'P', 'P', 'L', 'E']));
+// console.log(Adagrams.usesAvailableLetters('APPLES', ['A', 'P', 'P', 'L', 'E']));
+
 // console.log(Adagrams.drawLetters());
 
 // Do not remove this line or your tests will break!
