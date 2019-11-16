8d4e979c90788a04ee8d9d27feaf99c032aa3918
Mariya Burrows <mariya.burrows@gmail.com>
2019-11-15 17:20:04 -0800

    finished waves 2, 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -11,9 +11,58 @@ const Adagrams = {
       copyLetters.splice(index, 1);
     }
     return playersLetters;
+  },
+
+  usesAvailableLetters(word, lettersInHand) {
+    let handValues = {};
+
+    lettersInHand.forEach(function(letter){
+      if (handValues[letter] == null){
+        handValues[letter] = 1
+      } else {
+        handValues[letter] += 1
+      }
+    });
+
+    let valid = true; 
+
+    word.split('').forEach(function(letter){
+      if ((handValues[letter] === undefined) || (handValues[letter] === 0)){
+        valid = false
+      } 
+      else {
+       handValues[letter] -= 1
+       console.log(handValues)
+      };
+    })
+    return valid
+  },
+
+  scoreWord(word){
+    const letterScore = {
+      A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+      D: 2, G: 2, B: 3, C: 3, M: 3, P: 3, F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5, J: 8, X: 8, Q: 10, Z: 10
+    };
+
+    let score = 0;
+    const bonusPoints = [7, 8, 9, 10];
+
+    if (bonusPoints.includes(word.length)) {
+      score += 8
+    }
+
+    word.toUpperCase().split('').forEach(function(letter){
+      score += letterScore[letter]
+    });
+
+    return score
+
   }
 
 };
 console.log(Adagrams.drawLetters())
 // Do not remove this line or your tests will break!
 export default Adagrams;
+
+
