b8f58f840554afd38433ba7cbc3d52fbb14dc3cb
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-16 14:53:15 -0800

    created scoreword function, all tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -22,6 +22,21 @@ const Adagrams = {
     return result
   },
 
+  scoreWord(word) {
+    const score = { A:1, E:1, I:1, O:1, U:1, L:1, N:1, R:1, S:1, T:1, D:2, G:2, B:3, C:3, M:3, P:3, F:4, H:4, V:4, W:4, Y:4, K:5, J:8, X:8, Q:10, Z:10 } 
+    let points = 0
+
+    let wordCheck = word.toUpperCase();
+
+    if (wordCheck.length > 6) {
+      points += 8;
+    }
+
+    for (let i = 0; i < (wordCheck.length); i++) {
+        points += score[wordCheck[i]]
+    }
+      return points
+  },
 };
 
 // console.log(Adagrams.scoreWord('AVAILABLE'))
