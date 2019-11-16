187449c9627a60bf8842beffaf84a75ad1523a2d
Sam <samcoll@Samanthas-MBP.localdomain>
2019-11-16 14:51:59 -0800

    created drawletters function

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,15 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    const alphabet = 'AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ';
+    const lettersInHand = [];
+
+    for (let i = 0; i < 10; i++) {
+      lettersInHand.push(alphabet.charAt(Math.floor(Math.random() * alphabet.length)));
+    }
+    return lettersInHand;
   },
-};
+
+// console.log(Adagrams.drawLetters());
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
