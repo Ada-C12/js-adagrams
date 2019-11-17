44015976d6d0e6effbff2db9132757e0fb562c8b
ohcloud <cloudylopez@icloud.com>
2019-11-17 12:13:32 -0800

    made tiles into an object. working on pulling 10 tiles next. note, i got very lost in the syntax and what i thought wasnt working at all was working all along

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,23 @@
-const Adagrams = {
+const allLetters = {
+  A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12, R: 6, F: 2, S: 4, G: 3, T: 6, H: 2, U: 4, I: 9, V: 2, J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2, M: 2, Z: 1 } //this is now an object
+
+const Adagrams = { //this is wave one
   drawLetters() {
-    // Implement this method for wave 1
-  },
-};
+    let letters = [];
+    for(let letter in allLetters) {
+      for(let i = 0; i < allLetters[letter]; i += 1) {
+        letters.push(letter);
+      }
+    }
+    return letters;   
+  }
+}
+
+console.log(Adagrams.drawLetters())
+
+
+
+
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
