4970c16f80eff5db716949c32a78805669db756e
Rinostar <codingrinostar@gmail.com>
2019-11-16 18:30:16 -0800

    wave 3: implement 'scoreWord' function and pass tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -69,6 +69,56 @@ const Adagrams = {
     }
     return true
   },
+
+  scoreChart : {
+    A : 1,
+    E : 1,
+    I : 1,
+    O : 1,
+    U : 1,
+    L : 1,
+    N : 1,
+    R : 1,
+    S : 1,
+    T : 1,
+    D : 2,
+    G : 2,
+    B : 3,
+    C : 3,
+    M : 3,
+    P : 3,
+    F : 4,
+    H : 4,
+    V : 4,
+    W : 4,
+    Y : 4,
+    K : 5,
+    J : 8,
+    X : 8,
+    Q : 10,
+    Z : 10
+  },
+
+  scoreWord(word) {
+    const final_word = word.toUpperCase();
+    let score = 0;
+    let m  = 0;
+    while (m < final_word.length) {
+      score += this.scoreChart[final_word.charAt(m)];
+      m += 1;
+    }
+
+    switch (final_word.length) {
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+        score += 8;
+        break;
+    }
+
+    return score
+  },
 }
 
 // Do not remove this line or your tests will break!
