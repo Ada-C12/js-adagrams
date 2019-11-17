f6d0e66eef54ba22a9264fe80aba06a52176be98
Rinostar <codingrinostar@gmail.com>
2019-11-16 16:34:48 -0800

    set up data structure

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,37 @@
+// 1. Set up array data structure:
+const pool = Array(9).fill('A').concat(
+  Array(2).fill('B'), 
+  Array(2).fill('C'),
+  Array(4).fill('D'),
+  Array(12).fill('E'),
+  Array(2).fill('F'),
+  Array(3).fill('G'),
+  Array(2).fill('H'),
+  Array(9).fill('I'),
+  Array(1).fill('J'),
+  Array(1).fill('K'),
+  Array(4).fill('L'),
+  Array(2).fill('M'),
+  Array(6).fill('N'),
+  Array(8).fill('O'),
+  Array(2).fill('P'),
+  Array(1).fill('Q'),
+  Array(6).fill('R'),
+  Array(4).fill('S'),
+  Array(6).fill('T'),
+  Array(4).fill('U'),
+  Array(2).fill('V'),
+  Array(2).fill('W'),
+  Array(1).fill('X'),
+  Array(2).fill('Y'),
+  Array(1).fill('Z')
+);
+let POOL = pool;
+
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    letters_in_hand = POOL[Math.floor(Math.random() * POOL.length)].slice(0, 9);
+    return letters_in_hand
   },
 };
 
