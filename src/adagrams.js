aa19ae7c5b8d6d40c376697c7155263478c137ac
ohcloud <cloudylopez@icloud.com>
2019-11-17 19:02:03 -0800

    worked out the math for score word

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,12 @@
 let allLetters = {
-  A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12, R: 6, F: 2, S: 4, G: 3, T: 6, H: 2, U: 4, I: 9, V: 2, J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2, M: 2, Z: 1 }; //this is now an object
+  A: 9, N: 6, B: 2, O: 8, C: 2, P: 2, D: 4, Q: 1, E: 12, R: 6, F: 2, S: 4, G: 3, T: 6, H: 2, U: 4, I: 9, V: 2, J: 1, W: 2, K: 1, X: 1, L: 4, Y: 2, M: 2, Z: 1 }; 
+
+  let letterScores = {
+     A : 1, E : 1, I : 1, O : 1, U : 1, L : 1, N : 1,
+     R : 1, S : 1, T : 1, D : 2, G : 2, B : 3, C : 3,
+     M : 3, P : 3, F : 4, H : 4, V : 4, W : 4, Y : 4,
+     K : 5, J : 8, X : 8, Q : 10, Z : 10 
+  };
 
   const Adagrams = { 
     drawLetters() {//doesnt work with this, sorry
@@ -43,22 +50,27 @@ let allLetters = {
       } else if (!howManyTimes[wordInput[i]]) {
         return false;
       }
-      return true;
     }
+    return true;
+    },
 
-      
-      
-    
-    }
-
+  scoreWord(word) {
+    let thisTotal = 0;
+    const newWord = word.toUpperCase().split("");
+      newWord.forEach((letter) => {
+        thisTotal += letterScores[letter];
+      });
+      if (word.length >= 7) {
+        return thisTotal + 8;
+      } else {
+        return thisTotal;
+      }
+  }
 }
-console.log(Adagrams.usesAvailableLetters("hellll", ["K","H","E","L","L","O","P","A"]));
+
+// console.log(Adagrams.usesAvailableLetters("hellll", ["K","H","E","L","L","O","P","A"]));
+// console.log(Adagrams.scoreWord("aaaaaaaaa"));
 // console.log(Adagrams.drawLetters())
 
 // Do not remove this line or your tests will break
 // export default Adagrams;
-
-    // const input = input.toUpperCase().split(""); //now an array
-    // const copyMyHand = [...myHand]; //copy of my hand
-
-    //   if (copyMyHand.includes(input[i])).pop
