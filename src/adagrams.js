03505051ab38ae134864e3641e1be6bed24808e7
v <vanessareigosa@gmail.com>
2019-11-17 18:54:06 -0800

    refactorings

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,21 +1,38 @@
 const Adagrams = {
-  drawLetters() {
-    const letters = { 
-      "A": 9, "B": 2,
-      "C": 2, "D": 4, 
-      "E": 12, "F": 2,
-      "G": 3, "H": 2,
-      "I": 9, "J": 1,
-      "K": 1, "L": 4,
-      "M": 2, "N": 6,
-      "O": 8, "P": 2,
-      "Q": 1, "R": 6,
-      "S": 4, "T": 6,
-      "U": 4, "V": 2,
-      "W": 2, "X": 1,
-      "Y": 2, "Z": 1 
-    };
+  letterQuantity : { 
+    "A": 9, "B": 2,
+    "C": 2, "D": 4, 
+    "E": 12, "F": 2,
+    "G": 3, "H": 2,
+    "I": 9, "J": 1,
+    "K": 1, "L": 4,
+    "M": 2, "N": 6,
+    "O": 8, "P": 2,
+    "Q": 1, "R": 6,
+    "S": 4, "T": 6,
+    "U": 4, "V": 2,
+    "W": 2, "X": 1,
+    "Y": 2, "Z": 1 
+  },
 
+  letterPoints : { 
+    "A": 1, "B": 3,
+    "C": 3, "D": 2, 
+    "E": 1, "F": 4,
+    "G": 2, "H": 4,
+    "I": 1, "J": 8,
+    "K": 5, "L": 1,
+    "M": 3, "N": 1,
+    "O": 1, "P": 3,
+    "Q": 10, "R": 1,
+    "S": 1, "T": 1,
+    "U": 1, "V": 4,
+    "W": 4, "X": 8,
+    "Y": 4, "Z": 10 
+  },
+
+  drawLetters() {
+    let letters = this.letterQuantity
     const keys = Object.keys(letters);
     const hand = [];
 
@@ -47,42 +64,18 @@ const Adagrams = {
     return true 
   },
 
-    //now we wanna check every letter in the word 
-    //for a letter, look up the matching letter in the Letters object above
-    //add its value (points) to the score
   scoreWord(word) {
-    const letters = { 
-      "A": 1, "B": 3,
-      "C": 3, "D": 2, 
-      "E": 1, "F": 4,
-      "G": 2, "H": 4,
-      "I": 1, "J": 8,
-      "K": 5, "L": 1,
-      "M": 3, "N": 1,
-      "O": 1, "P": 3,
-      "Q": 10, "R": 1,
-      "S": 1, "T": 1,
-      "U": 1, "V": 4,
-      "W": 4, "X": 8,
-      "Y": 4, "Z": 10 
-    };
-
     let score = 0;
-    
     (word.length > 6 && word.length < 11 ? score += 8 : score += 0);
 
-    // for (const letterTile of word) {
-      //do stuff 
-    // }
-    let w = word.toUpperCase()
-    w.split('').forEach(function(letterTile){
-      score += (letters[letterTile]);
-    });
-    return score 
+    let w = word.toUpperCase();
+    for (const letterTile of w){
+      score += (this.letterPoints[letterTile]);
+    }
+    return score
   },
 
 };
-
 
 // Do not remove this line or your tests will break!
 export default Adagrams;
